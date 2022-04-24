be6effdc3d4647ec6c05422d482df03827bf8d0a
birendra777 <97580931+birendra777@users.noreply.github.com>
2022-04-24 14:14:47 +0530

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import { Car } from "./components/Car";
+import { Garage } from "./components/Garage";
+import "./styles.css";
+
+export default function App() {
+  return (
+    <div className="App">
+      <Car />
+      <Garage />
+    </div>
+  );
+}
